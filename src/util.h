9795f7ab2ade4244ccc16cb46d33af664582bcca
Theo Paris <theoparisdesigns@gmail.com>
2021-05-15 21:59:49 -0700

    Removed join function

--- src/util.h
+++ src/util.h
@@ -13,5 +13,4 @@ using namespace std;
 string exec(const char *cmd);
 string unquote(string input);
 
-string join(char delimiter, vector<string> data);
 vector<string> split(char delimiter, string input);
